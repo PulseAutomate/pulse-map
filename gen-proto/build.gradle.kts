7fe66aa1dabd77b145154fdd9b787fc0d2747b3b
anicholls35 <anicholls41@gmail.com>
2025-08-23 22:45:15 +0100

    feat: add protobuf templates and generator for attributes, entities, and services

--- gen-proto/build.gradle.kts
+++ gen-proto/build.gradle.kts
@@ -1,3 +1,18 @@
+plugins {
+    alias(libs.plugins.protobuf)
+}
+
 dependencies {
+    implementation(project(":manifest"))
+
+    implementation(libs.protobuf.java)
+
     testImplementation(libs.junit.jupiter)
+    testImplementation(libs.assertj.core)
+}
+
+protobuf {
+    protoc {
+        artifact = libs.protoc.get().toString()
+    }
 }
